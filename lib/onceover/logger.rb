69c1f03a0c5452f8a3d244c2328a9781142e614f
Mitchell Chambers <mitchell.chambers@puppet.com>
2023-07-10 16:14:33 +1000

    add option for no color

--- lib/onceover/logger.rb
+++ lib/onceover/logger.rb
@@ -4,25 +4,25 @@ module Onceover::Logger
   def logger
     unless $logger
       # here we setup a color scheme called 'bright'
-      Logging.color_scheme(
-        'bright',
-        :levels => {
-          :debug => :cyan,
-          :info  => :green,
-          :warn  => :yellow,
-          :error => :red,
-          :fatal => [:white, :on_red]
-        }
-      )
+      
       if opts[:no_color]
         Logging.appenders.stdout(
           'stdout',
           :layout => Logging.layouts.pattern(
             :pattern      => '%l\t -> %m\n',
-          # :color_scheme => 'bright'
           )
         )
       else
+        Logging.color_scheme(
+          'bright',
+          :levels => {
+            :debug => :cyan,
+            :info  => :green,
+            :warn  => :yellow,
+            :error => :red,
+            :fatal => [:white, :on_red]
+          }
+        )
         Logging.appenders.stdout(
           'stdout',
           :layout => Logging.layouts.pattern(
@@ -32,7 +32,7 @@ module Onceover::Logger
         )
       end
     
-      $logger = Logging.logger['Nocolors']
+      $logger = Logging.logger['colors']
       $logger.add_appenders 'stdout'
       $logger.level = :info
     end
